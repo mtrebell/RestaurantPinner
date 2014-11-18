0ae883b5c91c286a5e3ea82da31a87fe5447eea7
Melissa Trebell <mtrebell@gmail.com>
2014-11-17 22:03:51 -0700

    Add usergrid

--- app/src/main/java/com/restaurantapp/phoneapp/restaurantpinner/MyApplication.java
+++ app/src/main/java/com/restaurantapp/phoneapp/restaurantpinner/MyApplication.java
@@ -6,5 +6,6 @@ import android.app.Application;
  * Created by fix on 11/11/2014.
  */
 public class MyApplication extends Application {
-    public UserGrid usergrid = new UserGrid("http://192.168.2.156:8080/pinApp/resturantApp");
+    //For test purposes only,should grab when login.
+    public UserGrid usergrid = new UserGrid("http://192.168.2.156:8080/pinApp/resturantApp","b5a9691a-6621-11e4-b8b3-495762708d3a","YWMt7ZfS1m2-EeShLilWrTDcswAAAUnd7TwUeNpz-kCqQ2xUm15_mI9Cl6d8fIE");
 }
